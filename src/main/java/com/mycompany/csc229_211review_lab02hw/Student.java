99d22a8cbb458ba0d2fe9f3c08e951773553151f
asadfiaz997 <101984317+asadfiaz997@users.noreply.github.com>
2025-09-14 14:55:35 +0500

    Update Student.java

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,13 +4,40 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
+// ToDo 1: Make this class a child of Person
+public class Student extends Person {
+	//to inherit the class we use extend
 	
-	// ToDo 2: Fix the resulting errors
+	private double gpa;
+	private String address;
 	
+	public Student(String name , int age){
+       super(name , (short) age);
+	}
+
 	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+	public double getGpa(){
+      return gpa;
+	}
+
+	public void setGpa(double gpa){
+     this.gpa = gpa;
+	}
+
+	 // Required methods from Person 
+    @Override
+    public String getAddress() {
+        return address;
+
+    @Override
+    public void setAddress(String address) {
+         this.address = address;
+    }
+
+    //  toString method
+    @Override
+    public String toString() {
+        return "Name: " + getName() + ", Age: " + getAge() + ", GPA: " + gpa;
+    }
 
 }
